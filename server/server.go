d5abd03c9176395d9c2280b8142bda0e7fa2a8d0
Matt Liegey <mtliegey@gmail.com>
2015-04-22 12:29:26 -0400

    Work work

--- server/server.go
+++ server/server.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-    // "io"
-    "fmt"
-    "reflect"
-    "net/http"
-    "bytes"
+	// "io"
+	"bytes"
 	"encoding/binary"
+	"fmt"
+	"net/http"
+	"reflect"
 
-    "golang.org/x/net/websocket"
+	"golang.org/x/net/websocket"
 )
-
-
-
 
 // buf := new(bytes.Buffer)
 // 	var pi float64 = math.Pi
@@ -20,29 +17,28 @@ import (
 
 // Echo the data received on the WebSocket.
 func EchoServer(ws *websocket.Conn) {
-    var i int32 = 42
-    buf := new(bytes.Buffer)
-    _ = binary.Write(buf, binary.LittleEndian, i)
-    _ = websocket.Message.Send(ws, buf.Bytes())
+	var i int32 = 42
+	buf := new(bytes.Buffer)
+	_ = binary.Write(buf, binary.LittleEndian, i)
+	_ = websocket.Message.Send(ws, buf.Bytes())
 
-    // var bindata = []byte{0,0,0,1, 0,0,1,0, 0,1,0,0, 1,0,0,0}
-    // _ = websocket.Message.Send(ws, bindata)
-    // io.Copy(ws, ws)
+	// var bindata = []byte{0,0,0,1, 0,0,1,0, 0,1,0,0, 1,0,0,0}
+	// _ = websocket.Message.Send(ws, bindata)
+	// io.Copy(ws, ws)
 }
 
 // This example demonstrates a trivial echo server.
 func main() {
-    fmt.Println(reflect.TypeOf(WorldData))
-    fmt.Println(WorldData)
+	fmt.Println(reflect.TypeOf(WorldData))
+	fmt.Println(WorldData)
 
+	fs := http.FileServer(http.Dir("../assets"))
+	http.Handle("/", fs)
 
-    fs := http.FileServer(http.Dir("../assets"))
-    http.Handle("/", fs)
-
-    http.Handle("/echo", websocket.Handler(EchoServer))
-    err := http.ListenAndServe(":12345", nil)
-    if err != nil {
-        panic("ListenAndServe: " + err.Error())
-    }
+	http.Handle("/echo", websocket.Handler(EchoServer))
+	err := http.ListenAndServe(":12345", nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
 
 }
--- server/world-data.go
+++ server/world-data.go
@@ -1,5 +1,5 @@
 package main
 
 var WorldData [][]uint8 = [][]uint8{[]uint8{
-    0, 0, 0,
+	0, 0, 0,
 }}
