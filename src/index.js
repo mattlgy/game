3c543f28b984df1ee7101bd8b42a33d6e51c57c0
Matt Liegey <mtliegey@gmail.com>
2015-05-05 14:08:51 -0400

    code cleanup

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import Block from './block'
 import World from './world'
 import Chunk from './chunk'
 
+import getPointerLock from './get-pointer-lock'
+
 import worldData from './world-data'
 
 
@@ -15,7 +17,6 @@ websocket.onopen = function(event) {
   var arr = new Int32Array(buffer)
   arr[0] = 13
   arr[1] = 42
-  // console.log(arr)
   websocket.send(arr)
 }
 websocket.onmessage = function(event) {
@@ -24,7 +25,6 @@ websocket.onmessage = function(event) {
   console.log(new Int32Array(event.data))
 }
 
-
 getPointerLock()
 
 var CHUNK_SIZE_X = 4
@@ -32,21 +32,9 @@ var CHUNK_SIZE_Y = 8
 var CHUNK_SIZE_Z = 4
 var BLOCKS_PER_CHUNK = CHUNK_SIZE_X * CHUNK_SIZE_Y * CHUNK_SIZE_Z
 
-// var blocks = []
-// window.blocks = blocks
-
 var scene = new THREE.Scene()
 var world = new World({})
 window.world = world
-
-// // create a point light
-// var pointLight = new THREE.PointLight(0xFFFFFF)
-// // set its position
-// pointLight.position.x = 10
-// pointLight.position.y = 50
-// pointLight.position.z = 130
-// // add to the scene
-// scene.add(pointLight)
 
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
 // camera.position.z = 5
@@ -61,11 +49,15 @@ var renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
+
+function getChunk (x, z) {
+  
+}
+
 var i = 0
 var x = 0
 var z = 0
 var chunk
-
 while (i < 9) {
   chunk = world.newChunk(x, z, worldData[i])
   scene.add(chunk)
@@ -93,77 +85,3 @@ function onFrame (timestamp) {
   requestAnimationFrame(onFrame)
 }
 requestAnimationFrame(onFrame)
-
-
-
-function getPointerLock () {
-  // http://www.html5rocks.com/en/tutorials/pointerlock/intro/
-	var havePointerLock = 'pointerLockElement' in document ||
-    'mozPointerLockElement' in document ||
-    'webkitPointerLockElement' in document
-
-	if (havePointerLock) {
-		var element = document.body
-
-		var pointerlockchange = function ( event ) {
-			if ( document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element ) {
-				// controlsEnabled = true;
-				// controls.enabled = true;
-        //
-				// blocker.style.display = 'none';
-			} else {
-				// controls.enabled = false;
-        //
-				// blocker.style.display = '-webkit-box';
-				// blocker.style.display = '-moz-box';
-				// blocker.style.display = 'box';
-        //
-				// instructions.style.display = '';
-			}
-		}
-
-		var pointerlockerror = function ( event ) {
-			instructions.style.display = '';
-		}
-
-		// Hook pointer lock state change events
-		document.addEventListener( 'pointerlockchange', pointerlockchange, false );
-		document.addEventListener( 'mozpointerlockchange', pointerlockchange, false );
-		document.addEventListener( 'webkitpointerlockchange', pointerlockchange, false );
-
-		document.addEventListener( 'pointerlockerror', pointerlockerror, false );
-		document.addEventListener( 'mozpointerlockerror', pointerlockerror, false );
-		document.addEventListener( 'webkitpointerlockerror', pointerlockerror, false );
-
-		document.body.addEventListener( 'click', function ( event ) {
-			// instructions.style.display = 'none';
-
-			// Ask the browser to lock the pointer
-			element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
-
-			if ( /Firefox/i.test( navigator.userAgent ) ) {
-				var fullscreenchange = function ( event ) {
-					if ( document.fullscreenElement === element || document.mozFullscreenElement === element || document.mozFullScreenElement === element ) {
-						document.removeEventListener( 'fullscreenchange', fullscreenchange );
-						document.removeEventListener( 'mozfullscreenchange', fullscreenchange );
-
-						element.requestPointerLock();
-					}
-
-				}
-
-				document.addEventListener( 'fullscreenchange', fullscreenchange, false );
-				document.addEventListener( 'mozfullscreenchange', fullscreenchange, false );
-
-				element.requestFullscreen = element.requestFullscreen || element.mozRequestFullscreen || element.mozRequestFullScreen || element.webkitRequestFullscreen;
-
-				element.requestFullscreen();
-			} else {
-				element.requestPointerLock();
-			}
-		}, false );
-	} else {
-		document.body.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API';
-
-	}
-}
